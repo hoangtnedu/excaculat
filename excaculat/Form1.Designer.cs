6271a056c16bddd963fa6a9877ab0671a61d991b
HoangTN <HoangTN@DESKTOP-H68FGFI>
2022-10-22 11:05:40 +0700

    Add project files.

---
+++ excaculat/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace excaculat
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtKetqua = new System.Windows.Forms.TextBox();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btnbang = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtKetqua
+            // 
+            this.txtKetqua.Location = new System.Drawing.Point(78, 27);
+            this.txtKetqua.Name = "txtKetqua";
+            this.txtKetqua.Size = new System.Drawing.Size(287, 23);
+            this.txtKetqua.TabIndex = 0;
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(78, 80);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(44, 27);
+            this.btn0.TabIndex = 1;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(128, 80);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(44, 27);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(178, 80);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(44, 27);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btnbang
+            // 
+            this.btnbang.Location = new System.Drawing.Point(228, 80);
+            this.btnbang.Name = "btnbang";
+            this.btnbang.Size = new System.Drawing.Size(44, 27);
+            this.btnbang.TabIndex = 1;
+            this.btnbang.Text = "=";
+            this.btnbang.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 407);
+            this.Controls.Add(this.btnbang);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.txtKetqua);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtKetqua;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btnbang;
+    }
+}
+
---
+++ excaculat/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace excaculat
+{
+    public partial class Form1 : Form
+    {
+        string ketqua = "0";
+        public Form1()
+        {
+            InitializeComponent();
+            Display();
+        }
+
+        void Display()
+        {
+            txtKetqua.Text = ketqua;
+        }
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (ketqua=="0")
+            {
+                ketqua = "1";               
+            }
+            else
+            ketqua += "1";
+            Display();
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if (ketqua == "0")
+            {
+                ketqua = "2";               
+            }
+            else
+            ketqua += "2";
+            Display();
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            if (ketqua == "0")
+            {
+  
+            }
+            else
+            ketqua += "0";
+            Display();
+        }
+    }
+}
